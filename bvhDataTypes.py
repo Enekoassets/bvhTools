d85d773e21ccf726c4de1ebe1eaa0183c278d358
enekoatxa <eneko.atxa@opendeusto.es>
2025-03-25 10:56:05 +0100

    First commit with basic functionalities

---
+++ bvhDataTypes.py
@@ -0,0 +1,204 @@
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+
+class Joint:
+    def __init__(self, name, offset, channels, parent=None):
+        self.name = name
+        self.offset = offset 
+        self.channels = channels
+        self.children = []
+        self.parent = parent
+
+    def set_offset(self, offset):
+        self.offset = offset
+
+    def set_channels(self, channels):
+        self.channels = channels
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def get_channel_count(self):
+        return len(self.channels)
+
+class Skeleton:
+    def __init__(self, root_joint):
+        self.root = root_joint
+        self.joints = self.build_joint_dict(root_joint)
+        self.joint_indexes = self.build_joint_index_dict(root_joint, [0])
+
+    def build_joint_dict(self, joint):
+        joint_dict = {joint.name: joint}
+        for child in joint.children:
+            joint_dict.update(self.build_joint_dict(child))
+        return joint_dict
+
+    def build_joint_index_dict(self, joint, current_channel_index=[0]):
+        joint_index_dict = {joint.name: current_channel_index[0]}
+        current_channel_index[0] += joint.get_channel_count()
+
+        for child in joint.children:
+            joint_index_dict.update(self.build_joint_index_dict(child, current_channel_index))
+        return joint_index_dict
+
+    def get_joint(self, joint_name):
+        return self.joints[joint_name]
+
+    def get_joint_index(self, joint_name):
+        return self.joint_indexes[joint_name]
+
+class MotionData:
+    def __init__(self, num_frames, frame_time, frames):
+        self.num_frames = num_frames
+        self.frame_time = frame_time
+        self.frames = frames
+
+    def add_frame(self, frame_data):
+        self.frames.append(frame_data)
+
+    def get_frame(self, frame_index):
+        return self.frames[frame_index]
+    
+    def get_frame_slice(self, start_frame, end_frame):
+        return self.frames[start_frame:end_frame]
+
+    def get_rotations(self, rotation_index):
+        return [x[rotation_index] for x in self.frames]
+
+    def get_rotations_slice(self, start_index, end_index):
+        return [x[start_index:end_index] for x in self.frames]
+
+    def get_rotation_at_frame(self, rotation_index, frame):
+        return self.frames[frame][rotation_index]
+    
+    def get_rotation_slice_at_frame(self, start_index, end_index, frame):
+        return self.frames[frame][start_index:end_index]
+
+    def get_rotation_and_frame_slice(self, start_index, end_index, start_frame, end_frame):
+        return self.frames[start_frame, end_frame][start_index:end_index]
+
+class BVHData:
+    def __init__(self, skeleton, motion, header):
+        self.header = header
+        self.skeleton = skeleton
+        self.motion = motion
+        self.skeleton_dims = self.calculate_skeleton_dims()
+
+    def get_joint_local_transform_at_frame(self, joint_name, frame, rotationMode = "Euler"):
+        joint = self.skeleton.get_joint(joint_name)
+        jointIndex = self.skeleton.get_joint_index(joint_name)
+        r = None
+        Xrot, Yrot, Zrot = None, None, None
+        Xpos, Ypos, Zpos = 0.0, 0.0, 0.0
+        if("Xrotation" in joint.channels and "Yrotation" in joint.channels and "Zrotation" in joint.channels):
+            Xrot = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Xrotation"), frame)
+            Yrot = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Yrotation"), frame)
+            Zrot = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Zrotation"), frame)
+            r = R.from_euler('xyz', [Xrot, Yrot, Zrot], degrees=True)
+        if("Xposition" in joint.channels and "Yposition" in joint.channels and "Zposition" in joint.channels):
+            Xpos = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Xposition"), frame)
+            Ypos = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Yposition"), frame)
+            Zpos = self.motion.get_rotation_at_frame(jointIndex + joint.channels.index("Zposition"), frame)
+
+        if(r is None):
+            if(rotationMode == "Euler"):
+                return R.identity().as_euler('xyz', degrees=True), [Xpos, Ypos, Zpos]
+            if(rotationMode == "Quaternion"):
+                return R.identity().as_quat(), [Xpos, Ypos, Zpos]
+            if(rotationMode == "Matrix"):
+                return R.identity().as_matrix(), [Xpos, Ypos, Zpos]
+        else:
+            if(rotationMode == "Euler"):
+                return r.as_euler('xyz', degrees=True), [Xpos, Ypos, Zpos]
+            if(rotationMode == "Quaternion"):
+                return r.as_quat(), [Xpos, Ypos, Zpos]
+            if(rotationMode == "Matrix"):
+                return r.as_matrix(), [Xpos, Ypos, Zpos]
+
+    def calculate_skeleton_dims(self):
+        min_x, min_y, min_z = float('inf'), float('inf'), float('inf')
+        max_x, max_y, max_z = float('-inf'), float('-inf'), float('-inf')
+
+        fk_data_0 = self.get_FK_at_frame(0)
+        for joint_name, (rot, pos) in fk_data_0.items():
+            # Extract the position of each joint
+            x, y, z = pos
+            
+            # Update the min and max values for each axis (X, Y, Z)
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+            min_z = min(min_z, z)
+
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+            max_z = max(max_z, z)
+
+        # Calculate height, width, and depth
+        height = max_y - min_y  # Difference in the Y-axis (vertical)
+        width = max_x - min_x   # Difference in the X-axis (horizontal)
+        depth = max_z - min_z   # Difference in the Z-axis (depth)
+
+        return [height, width, depth]
+
+    def get_skeleton_dim(self, dimName):
+        if(dimName == "width"):
+            return self.skeleton_dims[0]
+        if(dimName == "height"):
+            return self.skeleton_dims[1]
+        if(dimName == "depth"):
+            return self.skeleton_dims[2]
+
+    def get_skeleton_dims(self):
+        return self.skeleton_dims
+
+    def get_child_FK_at_frame(self, joint, frame, parent_transform, fk_frame):
+        local_rot, local_pos = self.get_joint_local_transform_at_frame(joint.name, frame, "Matrix")
+        joint_global_rot = np.matmul(parent_transform[0], local_rot)
+        rotated_offset = np.matmul(parent_transform[0], joint.offset)
+        joint_global_pos = np.add(np.add(rotated_offset, local_pos), parent_transform[1])
+        fk_frame.update({joint.name: (joint_global_rot, joint_global_pos)})
+        for child in joint.children:
+            self.get_child_FK_at_frame(child, frame, (joint_global_rot, joint_global_pos), fk_frame)
+
+    def get_FK_at_frame(self, frame):
+        root_joint = self.skeleton.root
+        root_local_rot, root_local_pos = self.get_joint_local_transform_at_frame(root_joint.name, frame, "Matrix")
+        fk_frame = {root_joint.name: (root_local_rot, root_local_pos)}
+        for child in root_joint.children:
+            self.get_child_FK_at_frame(child, frame, (root_local_rot, root_local_pos), fk_frame)
+        return fk_frame
+    
+    def get_FK_at_frame_normalized(self, frame, skeletonDim = "height"):
+        fk_frame = self.get_FK_at_frame(frame)
+        normalizer = self.get_skeleton_dim(skeletonDim)
+        for joint_name, (rot, pos) in fk_frame.items():
+            fk_frame[joint_name] = (rot, pos / normalizer)
+        return fk_frame
+    
+    def center_skeleton_root(self, fk_frame=0):
+        frame = self.motion.get_frame(fk_frame)
+        rootIndex = self.skeleton.get_joint_index(self.skeleton.root.name)
+        offsets = [-float(frame[rootIndex]), -float(frame[rootIndex + 1]), -float(frame[rootIndex + 2])]
+        for frame in self.motion.frames:
+            frame[rootIndex] += offsets[0]
+            frame[rootIndex+1] += offsets[1]
+            frame[rootIndex+2] += offsets[2]
+
+    def center_skeleton_feet(self, fk_frame=0, leftFootName = "LeftFoot", rightFootName = "RightFoot"):
+        if(leftFootName not in self.skeleton.joints):
+            raise Exception("Left foot not found in skeleton")
+        if(rightFootName not in self.skeleton.joints):
+            raise Exception("Right foot not found in skeleton")
+
+        avgFootHeight = (self.get_FK_at_frame(fk_frame)[leftFootName][1][1] + self.get_FK_at_frame(fk_frame)[rightFootName][1][1]) / 2
+        avgRootHeight = self.get_FK_at_frame(fk_frame)[self.skeleton.root.name][1][1]
+        frame = self.motion.get_frame(fk_frame)
+        rootIndex = self.skeleton.get_joint_index(self.skeleton.root.name)
+        offsets = [-float(frame[rootIndex]), -float(frame[rootIndex + 1]) + (avgRootHeight - avgFootHeight), -float(frame[rootIndex + 2])]
+        for frame in self.motion.frames:
+            frame[rootIndex] += offsets[0]
+            frame[rootIndex+1] += offsets[1]
+            frame[rootIndex+2] += offsets[2]
---
+++ bvhIO.py
@@ -0,0 +1,105 @@
+from bvhDataTypes import Joint, Skeleton, MotionData, BVHData
+
+def buildBvhStructure(header, motion, num_frames, frame_time):
+    current_index = 0
+    root_joint = None
+    while(current_index < len(header)):
+        if("ROOT" in header[current_index]):
+            root_joint, newIndex = readJoint(header, current_index)
+            current_index = newIndex
+            break
+        current_index += 1
+    skeleton = Skeleton(root_joint)
+    motionData = MotionData(num_frames=num_frames, frame_time=frame_time, frames = motion)
+    bvh = BVHData(skeleton=skeleton, motion=motionData, header = header)
+    return bvh
+
+def readEndSite(header, current_index, parent):
+    current_index += 1
+    while(current_index < len(header)):
+        if("{" in header[current_index]):
+            current_index += 1
+        if("OFFSET" in header[current_index]):
+            offset = [float(x) for x in header[current_index].split(" ")[1:]]
+            current_index += 1
+        if("}" in header[current_index]):
+            current_index += 1
+            break
+    
+    endSite = Joint(name = f"{parent.name}_EndSite", offset=offset, channels = [], parent=parent)
+
+    return endSite, current_index
+        
+def readJoint(header, current_index, parent=None):
+    jointName = header[current_index].split(" ")[1]
+    current_index += 1
+    jointObject = Joint(name = jointName, offset=None, channels=[], parent = parent)
+
+    while(current_index < len(header)):
+        if("{" in header[current_index]):
+            current_index += 1
+        
+        if("OFFSET" in header[current_index]):
+            jointObject.set_offset([float(x) for x in header[current_index].rstrip().split(" ")[1:]])
+            current_index += 1
+        
+        if("CHANNELS" in header[current_index]):
+            jointObject.set_channels([str(x) for x in header[current_index].rstrip().split(" ")[2:]])
+            current_index += 1
+        
+        if("JOINT" in header[current_index]):
+            childJoint, current_index = readJoint(header, current_index, jointObject)
+            jointObject.add_child(childJoint)
+
+        if("End Site" in header[current_index]):
+            endSite, current_index = readEndSite(header, current_index, jointObject)
+            jointObject.add_child(endSite)
+        
+        if("}" in header[current_index]):
+            current_index += 1
+            break
+
+    return jointObject, current_index
+
+def readBvhFile(bvhPath):
+    header = []
+    motion = []
+    num_frames = 0
+    frame_time = 0.0
+
+    with open(bvhPath, "r") as f:
+        # read and process the header
+        line = f.readline()
+        while(True):
+            if("MOTION" in line):
+                break
+            header.append(line.strip("\n"))
+            line = f.readline()
+
+        # read and process the motion data
+        line = f.readline()
+        while(line != ""):
+            if("Frames:" in line):
+                num_frames = int(line.split(" ")[1])
+            elif("Frame Time:" in line):
+                frame_time = float(line.split(" ")[2])
+            else:
+                motion.append([float(x) for x in line.rstrip().replace("\n", "").split(" ")])
+            line = f.readline()
+    bvhData = buildBvhStructure(header, motion, num_frames, frame_time)
+    return bvhData
+
+def writeBvhFile(bvhData, bvhPath, decimals = 6):
+    with open(bvhPath, "w") as f:
+        for line in bvhData.header:
+            f.write(line)
+            f.write("\n")
+        f.write("\n")
+        f.write("MOTION\n")
+        f.write("Frames: " + str(bvhData.motion.num_frames) + "\n")
+        f.write("Frame Time: " + str(bvhData.motion.frame_time) + "\n")
+        for frame in bvhData.motion.frames:
+            strings = [f"{x:.6f}" for x in frame]
+            for string in strings:
+                f.write(string + " ")
+            f.write("\n")
