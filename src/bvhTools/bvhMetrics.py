50ef3d6776dee004edb0746b05e02c50fee5f0a8
enekoatxa <eneko.atxa@opendeusto.es>
2025-11-24 16:14:14 +0100

    added angular speed, acceleration, jerk calculations. unified averaging perFrame and perJoint

--- src/bvhTools/bvhMetrics.py
+++ src/bvhTools/bvhMetrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial.transform import rotation as R
+from scipy.spatial.transform import Rotation as R
 
 def getSpeedVectors(bvh, timeDiff = -1):
     if bvh.motion.numFrames < 2:
@@ -80,65 +80,127 @@ def getJerks(bvh, timeDiff = -1):
     allJerks = getJerkVectors(bvh, timeDiff)
     return np.linalg.norm(allJerks, axis = 2)
 
-def getAvgSpeeds(bvh, timeDiff = -1):
+def getAvgSpeeds(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 2:
         return np.empty(0)
 
     allSpeeds = getSpeeds(bvh, timeDiff)
-    return np.mean(allSpeeds, axis = 0)
+    return np.mean(allSpeeds, axis = axis)
 
-def getAvgAccelerations(bvh, timeDiff = -1):
+def getAvgAccelerations(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 3:
         return np.empty(0)
 
     allAccelerations = getAccelerations(bvh, timeDiff)
-    return np.mean(allAccelerations, axis = 0)
+    return np.mean(allAccelerations, axis = axis)
 
-def getAvgJerks(bvh, timeDiff = -1):
+def getAvgJerks(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 4:
         return np.empty(0)
 
     allJerks = getJerks(bvh, timeDiff)
-    return np.mean(allJerks, axis = 0)
+    return np.mean(allJerks, axis = axis)
 
-def getAvgSpeedsPerFrame(bvh, timeDiff = -1):
+def getAngularSpeedVectors(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 2:
+        return np.empty((0,0,0))
+    allFrameRotations = []
+    allSpeeds = []
+    rotations = []
+    if timeDiff == -1:
+        frameTime = bvh.motion.frameTime
+    else:
+        frameTime = timeDiff
+
+    for frameIndex in range(bvh.motion.numFrames):
+        for jointName in bvh.skeleton.joints:
+            if("EndSite" in jointName):
+                continue
+            joint = bvh.skeleton.getJoint(jointName)
+            motionIndex = joint.motionIndex
+            if joint.getChannelCount() == 6 and (joint.channels[0] == "Xrotation" or joint.channels[0] == "Yrotation" or joint.channels[0] == "Zrotation"):
+                motionIndex += 3
+            rotations.append(R.from_euler(joint.getRotationChannelsOrder(), bvh.motion.frames[frameIndex][motionIndex:motionIndex+3], degrees=True))
+        allFrameRotations.append(rotations)
+        rotations = []
+    allFrameRotations = np.asarray(allFrameRotations)
+    for frameIndex in range(1, len(allFrameRotations)):
+        allSpeeds.append([(r2 * r1.inv()).as_rotvec()/frameTime for r1, r2 in zip(allFrameRotations[frameIndex - 1], allFrameRotations[frameIndex])])
+
+    return np.asarray(allSpeeds)
+
+def getAngularAccelerationVectors(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 3:
+        return np.empty((0,0,0))
+    allSpeeds = getAngularSpeedVectors(bvh, timeDiff)
+    if timeDiff == -1:
+        frameTime = bvh.motion.frameTime
+    else:
+        frameTime = timeDiff
+    allAccelerations = []
+    for frameIndex in range(1, len(allSpeeds)):
+        allAccelerations.append([(r2 - r1)/frameTime for r1, r2 in zip(allSpeeds[frameIndex - 1], allSpeeds[frameIndex])])
+
+    return np.asarray(allAccelerations)
+
+def getAngularJerkVectors(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 4:
+        return np.empty((0,0,0))
+    allAccelerations = getAngularAccelerationVectors(bvh, timeDiff)
+    if timeDiff == -1:
+        frameTime = bvh.motion.frameTime
+    else:
+        frameTime = timeDiff
+    allJerks = []
+    for frameIndex in range(1, len(allAccelerations)):
+        allJerks.append([(r2 - r1)/frameTime for r1, r2 in zip(allAccelerations[frameIndex - 1], allAccelerations[frameIndex])])
+
+    return np.asarray(allJerks)
+
+def getAngularSpeeds(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 2:
+        return np.empty((0,0))
+    allSpeeds = getAngularSpeedVectors(bvh, timeDiff)
+    return np.linalg.norm(allSpeeds, axis = 2)
+
+def getAngularAccelerations(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 3:
+        return np.empty((0,0))
+    allAccelerations = getAngularAccelerationVectors(bvh, timeDiff)
+    return np.linalg.norm(allAccelerations, axis = 2)
+
+def getAngularJerks(bvh, timeDiff = -1):
+    if bvh.motion.numFrames < 4:
+        return np.empty((0,0))
+    allJerks = getAngularJerkVectors(bvh, timeDiff)
+    return np.linalg.norm(allJerks, axis = 2)
+
+def getAvgAngularSpeeds(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 2:
         return np.empty(0)
 
-    allSpeeds = getSpeeds(bvh, timeDiff)
-    return np.mean(allSpeeds, axis = 1)
+    allSpeeds = getAngularSpeeds(bvh, timeDiff)
+    return np.mean(allSpeeds, axis = axis)
 
-def getAvgAccelerationsPerFrame(bvh, timeDiff = -1):
+def getAvgAngularAccelerations(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 3:
         return np.empty(0)
 
-    allAccelerations = getAccelerations(bvh, timeDiff)
-    return np.mean(allAccelerations, axis = 1)
+    allAccelerations = getAngularAccelerations(bvh, timeDiff)
+    return np.mean(allAccelerations, axis = axis)
 
-def getAvgJerksPerFrame(bvh, timeDiff = -1):
+def getAvgAngularJerks(bvh, timeDiff = -1, mode = "perJoint"):
+    axis = 0 if mode == "perJoint" else 1
     if bvh.motion.numFrames < 4:
         return np.empty(0)
 
-    allJerks = getJerks(bvh, timeDiff)
-    return np.mean(allJerks, axis = 1)
-
-def getAngularSpeedVectors(bvh):
-    pass
-
-def getAngularAccelerationVectors(bvh):
-    pass
-
-def getAngularJerkVectors(bvh):
-    pass
-
-def getAvgAngularSpeeds(bvh):
-    pass
-
-def getAvgAngularAccelerations(bvh):
-    pass
-
-def getAvgAngularJerks(bvh):
-    pass
+    allJerks = getAngularJerks(bvh, timeDiff)
+    return np.mean(allJerks, axis = axis)
 
 def getFootContactsSpeedMethod(bvh, footNames = ["LeftFoot", "RightFoot"], threshold = 0.1, timeDiff = -1):
     speedsPerFrame = getSpeeds(bvh, timeDiff)
